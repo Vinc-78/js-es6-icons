02d850a6887bf94284e88d8ac802f72cd5b642d4
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-26 17:30:02 +0200

    creata una funzione per dividere le categorie

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ const coloriIcone= {
 }
 
 
+
+
 // realizzo un ciclo per stampare tutte le cards
 
 for (let i = 0; i < iconsList.length; i++) {
@@ -36,7 +38,6 @@ stampaCard(name,prefix,family,color);
 
 
 
-
 // funzione esterna per realizzare l'HTML
 
 function stampaCard(arg1, arg2, arg3, arg4) {
@@ -55,3 +56,69 @@ function stampaCard(arg1, arg2, arg3, arg4) {
 </div>`
 
 }
+
+
+// ho bisogno ora di una funzione che raggruppi le icone 
+//per tipo, family prefix e colore
+
+function raggruppoIcone(iconsList) {
+
+    //parto da un oggetto vuoto che avrà al sui interno
+    //tanti array quanti i tipi di icone, ognuno pieno dell'elenco di icone
+
+    const gruppoIcone = {}; // per ora è vuoto
+
+    for (let i = 0; i < iconsList.length; i++) {
+
+        const {type, name, prefix,family} =iconsList[i]; //passo i singoli elementi di iconList
+
+        const color = coloriIcone[type]; //passo il colore ( avrei potuto dichiarare entrambi in globale )
+
+         if(!gruppoIcone[type]) {
+             gruppoIcone[type]= [];  //se quel tipo/proprietà non c'è lo creo
+         }
+
+         gruppoIcone[type].push({
+             name,
+             prefix,
+             family,
+             color
+         }); 
+         
+    }
+
+    console.log(gruppoIcone);
+}
+
+const gruppoPerCategoria = raggruppoIcone(iconsList);
+
+//raggruppoIcone(iconsList); //chiamo la funzione per il console log
+
+
+
+
+
+function stampaUnaCategoria(lista) {
+
+    // realizzo un ciclo per stampare tutte le cards
+    
+    for (let i = 0; i < lista.length; i++) {
+        const oneCard = lista[i];
+    
+        //utilizzo il distracturing per lavorare con le proprietà 
+        //degli oggetti dell'array
+        const {name, prefix, family, type}=oneCard;
+    
+        const color = coloriIcone[type];
+    
+        // const color = coloriIcone[oneCard.type];
+        // const color = coloriIcone[iconlist[i].type];
+       
+    // creo l'HTML 
+    
+    stampaCard(name,prefix,family,color);
+    
+        
+    }
+    
+    }
