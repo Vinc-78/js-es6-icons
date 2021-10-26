197b902cf5683d47fdc2cf895675aab339f1322a
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-26 15:42:47 +0200

    ho colorato le icone

--- js/script.js
+++ js/script.js
@@ -2,24 +2,44 @@
 
 const cardContainer = document.querySelector(".card-container");
 
+//creo un oggetto per associare al tipo di icona il colore
+//lo posiziono prima del for per poter essere letto dal ciclo for
+
+const coloriIcone= {
+    animal:'blue',
+    vegetable:'orange',
+    user:'purple'
+
+}
+
+
 // realizzo un ciclo per stampare tutte le cards
 
 for (let i = 0; i < iconsList.length; i++) {
     const oneCard = iconsList[i];
 
+    //utilizzo il distracturing per lavorare con le proprietà 
+    //degli oggetti dell'array
+    const {name, prefix, family, type}=oneCard;
 
+    const color = coloriIcone[type];
+
+    // const color = coloriIcone[oneCard.type];
+   
+// reo l'HTML 
 
 cardContainer.innerHTML +=
 
 `<div class="col">
 <div class="card shadow-sm">
   <div class="card-img-top">
-    <i class="${oneCard.family} ${oneCard.prefix}${oneCard.name}" style="color: blue"></i>
+    <i class="${family} ${prefix}${name}" style="color: ${color}"></i>
   </div>
   <div class="card-body">
-    <div class="card-title text-center">${oneCard.name}</div>
+    <div class="card-title text-center">${name}</div>
   </div>
 </div>
 </div>`
     
 }
+
